bfd28736059f78130b5fc22d77a3fffd704df471
anyascode <145881071+anyascode@users.noreply.github.com>
2024-05-22 09:25:39 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,32 @@
+const start = Date.now();
+sessionStorage.setItem("start", String(start));
+
+const route = (event) => {
+  event = event || window.event;
+  event.preventDefault();
+  window.history.pushState({}, "", event.target.href);
+  handleLocation();
+};
+
+const routes = {
+  "/": "resume",
+  "/map": "map",
+  "/timerpage": "timerpage",
+};
+
+function showPage(page) {
+  document.querySelectorAll("#main > div").forEach((div) => {
+    div.style.display = "none";
+  });
+  document.getElementById(page).style.display = "block";
+  document.querySelector(".tabMenu").dataset.page = page;
+}
+
+function handleLocation() {
+  const path = window.location.pathname;
+  const page = routes[path];
+  showPage(page || "resume");
+}
+window.onpopstate = handleLocation;
+window.route = route;
+handleLocation();
